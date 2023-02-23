d5fea19ebd39eb9bdedd82de00872a108d50b1ec
Siarheyeu <115313319+siarheyeu@users.noreply.github.com>
2023-02-24 00:25:27 +0300

    Final

---
+++ src/main/java/ru/siarheyeu/springcourse/Project2Boot/repositories/BooksRepository.java
@@ -0,0 +1,16 @@
+package ru.siarheyeu.springcourse.Project2Boot.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.siarheyeu.springcourse.Project2Boot.models.Book;
+
+
+import java.util.List;
+
+/**
+ * @author Neil Alishev
+ */
+@Repository
+public interface BooksRepository extends JpaRepository<Book, Integer> {
+    List<Book> findByTitleStartingWith(String title);
+}
---
+++ src/main/java/ru/siarheyeu/springcourse/Project2Boot/util/PersonValidator.java
@@ -0,0 +1,36 @@
+package ru.siarheyeu.springcourse.Project2Boot.util;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+import ru.siarheyeu.springcourse.Project2Boot.models.Person;
+import ru.siarheyeu.springcourse.Project2Boot.services.PeopleService;
+
+
+/**
+ * @author Neil Alishev
+ */
+@Component
+public class PersonValidator implements Validator {
+
+    private final PeopleService peopleService;
+
+    @Autowired
+    public PersonValidator(PeopleService peopleService) {
+        this.peopleService = peopleService;
+    }
+
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return Person.class.equals(aClass);
+    }
+
+    @Override
+    public void validate(Object o, Errors errors) {
+        Person person = (Person) o;
+
+        if (peopleService.getPersonByFullName(person.getFullName()).isPresent())
+            errors.rejectValue("fullName", "", "Человек с таким ФИО уже существует");
+    }
+}
